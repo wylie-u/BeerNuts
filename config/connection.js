1a63babcfe262667caaecdcd5956017f148582dd
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-04-05 19:39:21 -0600

    commented out local host

--- config/connection.js
+++ config/connection.js
@@ -11,9 +11,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      // host: 'localhost',
+      // dialect: 'mysql',
+      // port: 3306
     }
   );
 }
