fc00faeadb5e7fd0c0010d832dfbb1d3d44d1847
Taylor Wright <tjwri25@gmail.com>
2021-04-07 19:07:49 -0600

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,34 @@ require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3306,
+
     });
 
 module.exports = sequelize;
+
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     }
+//   );
+// }
+
+// module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 // const routes = require('./routes');
 
-const sequelize = require('./config/connection');
+const sequelize = require('../project2/config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
