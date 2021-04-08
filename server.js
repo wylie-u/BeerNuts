7024630295a2d32a74afc89d90bdc8bde3ab709c
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-04-07 19:00:52 -0600

    Connection is now working

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,5 +12,5 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
-  });
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
