d768d0659f16a0566ced900f33d692db7eae1197
wylie-u <wylieurig@gmail.com>
2021-04-07 21:41:54 -0600

    updating home route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const homeRoutes = require('./homeRoutes');
+// const projectRoutes = require('./projectRoutes');
 
-router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/homeRoutes', userRoutes);
+// router.use('/projects', projectRoutes);
 
 module.exports = router;
