391f7f48f65e2560db329bc8bca38c69d34631ee
wylie-u <wylieurig@gmail.com>
2021-04-07 19:30:36 -0600

    adding controllers

--- config/connection.js
+++ config/connection.js
@@ -14,3 +14,16 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
+// module.exports = sequelize;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3301;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
