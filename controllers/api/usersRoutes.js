b5668e1c26217a5a85f33387462418b1f787d4e8
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-04-11 13:54:41 -0600

    Delete route created for user by id

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -59,4 +59,21 @@ router.post('/logout', (req, res) => {
   }
 });
 
+// Delete user by ID
+router.delete('/:id', async (req, res) => {
+  // delete one user by its `id` value
+  try {
+    const userData = await User.destroy({
+      where: { id: req.params.id },
+    });
+    if (!userData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
