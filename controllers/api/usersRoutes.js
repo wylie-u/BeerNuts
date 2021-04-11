8fa6b800dd6ebfa533694c79429962b96986a437
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-04-11 13:41:37 -0600

    User post routes working

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// sign up 
+// sign up
 router.post('/', async (req, res) => {
   try {
-    // data will be retreived here for new user info
+    // data will be retrieved here for new user info
     const userData = await User.create(req.body);
 
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-    // returning the userdata variable as json
+      // returning the userdata variable as json
       res.status(200).json(userData);
     });
   } catch (err) {
