4cce2df5e55afced7476e8040f815ffe9d076133
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-04-11 13:51:35 -0600

    Response for user post route is showing 200 status and sending back user json in response

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -6,13 +6,13 @@ router.post('/', async (req, res) => {
   try {
     // data will be retrieved here for new user info
     const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      // returning the userdata variable as json
-      res.status(200).json(userData);
-    });
+    // req.session.save(() => {
+    //   req.session.user_id = userData.id;
+    //   req.session.logged_in = true;
+    //   // returning the userdata variable as json
+    // });
+    res.status(200).json(userData);
+    console.log(res);
   } catch (err) {
     res.status(400).json(err);
   }
