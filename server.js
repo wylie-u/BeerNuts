e2120a93b4d87cfe07c5d0d8325dc848cc32d116
wylie-u <wylieurig@gmail.com>
2021-04-12 19:27:01 -0600

    working on auth

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const path = require('path');
 const express = require('express');
 // const routes = require('./routes');
 const routes = require('./controllers');
+const session = require('express-session');
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +14,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //static assets go here (CSS and javascript)
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.use(routes);
+
+
 
 
 sequelize.sync({ force: false }).then(() => {
