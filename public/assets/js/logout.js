fdeb6b6f3425563c73d41e5865403e59ad2588da
wylie-u <wylieurig@gmail.com>
2021-04-13 21:49:20 -0600

    signup route works with 200-need to fix loginroute

--- public/assets/js/logout.js
+++ public/assets/js/logout.js
@@ -1,5 +1,5 @@
 const logout = async () => {
-    const response = await fetch('/api/usersRoutes/logout', {
+    const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
