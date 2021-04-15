8df3c8d75e591921e05890861d2618135cbe3814
wylie-u <wylieurig@gmail.com>
2021-04-14 21:18:30 -0600

    updated logout js

--- public/assets/js/logout.js
+++ public/assets/js/logout.js
@@ -1,4 +1,5 @@
 const logout = async () => {
+  
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -12,4 +13,6 @@ const logout = async () => {
     }
   };
   
-  document.querySelector('#logout').addEventListener('click', logout);
+  
+  document.querySelector('.logout').addEventListener('click', logout);
+  
