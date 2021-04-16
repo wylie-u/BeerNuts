d27daa4c544ba9d2c562bee033102ef344583426
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-04-16 12:20:30 -0600

    halfway

--- public/assets/js/addbrewery.js
+++ public/assets/js/addbrewery.js
@@ -1,50 +1,47 @@
-var buttonsub= document.getElementById('addBrewery')
-console.log(buttonsub)
+var buttonSub = document.getElementById('addBrewery');
+
 // subBtn.addEventListener('submit', handleForm);
 function handleForm2(event) {
-    // event.preventDefault();
-    console.log("clicked here");
-    breweryForm();
-  }
+  event.preventDefault();
+  console.log('clicked here');
+  // newFormHandler();
+}
 
-  $("#submitNewBrewery").click(function(){
-    $(".card-addBrewery").hide();
-    
-  });
+$('#submitNewBrewery').click(function () {
+  $('.card-addBrewery').hide();
+});
 
-  const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const cityName= document.querySelector("#city_name").value.trim();
-    const breweryName= document.querySelector("#name").value.trim();
-    const location= document.querySelector("#location").value.trim();
-    const phNumber= document.querySelector("#phone_number").value.trim();
-    const website= document.querySelector("#website").value.trim();
-    const food= document.querySelector("#food").value.trim();
-    const outdoor= document.querySelector("#outdoor_seating").value.trim();
-  
+const newFormHandler = async (event) => {
+  event.preventDefault();
+  const thanksRow = document.getElementById('thankyou-row');
+  const cityName = document.querySelector('#city_name').value.trim();
+  const breweryName = document.querySelector('#name').value.trim();
+  const location = document.querySelector('#location').value.trim();
+  const phNumber = document.querySelector('#phone_number').value.trim();
+  const website = document.querySelector('#website').value.trim();
+  const food = document.querySelector('#food').value.trim();
+  const outdoor = document.querySelector('#outdoor_seating').value.trim();
+
   {
-      const response = await fetch(`/api/brewery`, {
-        method: 'POST',
-        body: JSON.stringify({
-            "city_name" : cityName,
-            "name" : breweryName,
-            "location": location,
-            "phone_number": phNumber,
-            "food": food,
-            "outdoor_seating": outdoor,
-            "website": website
-         }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      console.log("successful")
-  
-    }
-    // add .show/hide here
-    
-  };
+    const response = await fetch(`/api/brewery`, {
+      method: 'POST',
+      body: JSON.stringify({
+        city_name: cityName,
+        name: breweryName,
+        location: location,
+        phone_number: phNumber,
+        food: food,
+        outdoor_seating: outdoor,
+        website: website,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log('successful');
+  }
+  // add .show/hide here
+  thanksRow.classList.add('hide-form');
+};
 
-  buttonsub.addEventListener('submit', newFormHandler);
-
+buttonSub.addEventListener('submit', handleForm2);
